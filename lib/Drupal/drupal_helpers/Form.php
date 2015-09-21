8375cbd8bccee8a3eaa652b99ee09c84a88b358f
Alex Skrypnyk <alex.designworks@gmail.com>
2015-09-21 11:39:09 +1000

    Added form helpers.

---
+++ lib/Drupal/drupal_helpers/Form.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @file
+ * Form-related helpers.
+ */
+
+namespace Drupal\drupal_helpers;
+
+/**
+ * Class Form.
+ *
+ * @package Drupal\drupal_helpers
+ */
+class Form {
+  /**
+   * Get default values from the form.
+   *
+   * @param [] $params
+   *   Array of parameter values. Usually, taken from
+   *   drupal_get_query_parameters().
+   *
+   * @param ...
+   *   Variable number of keys to traverse $params.
+   *
+   * @param mixed $default
+   *   Default value to return if parameter does not exist.
+   *
+   * @return mixed|null
+   *   Returns one of the following values:
+   *   - value: Parameter value if all provided keys exist.
+   *   - default value: Value of $default if one or more keys do not exist in
+   *     $params.
+   *   - NULL: If this function was called with insufficient number of
+   *     arguments.
+   */
+  static public function formGetDefaults() {
+    $args = func_get_args();
+
+    if (count($args) < 3) {
+      return NULL;
+    }
+
+    $params = array_shift($args);
+    $defaults = array_pop($args);
+    $keys = $args;
+
+    $val = $params;
+    foreach ($keys as $key) {
+      if (isset($val[$key])) {
+        $val = $val[$key];
+      }
+      else {
+        $val = $defaults;
+        break;
+      }
+    }
+
+    return $val;
+  }
+}
